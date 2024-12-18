dec9ba00eeb7705a6a22422d8caf48d8d38f58b0
Sashakrm <sasakramch@gmail.com>
2024-12-18 09:41:43 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Func();
+            Console.Read();
+            
+        }
+        static void Func()
+        {
+            for (int x = 1; x <= 25; x++) 
+            {
+                Console.Write("@");
+            }
+        }
+    }
+}
