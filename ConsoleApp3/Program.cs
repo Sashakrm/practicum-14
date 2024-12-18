515c176203018d2b8869c7e9c3574cda93f36496
Sashakrm <sasakramch@gmail.com>
2024-12-18 09:28:22 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            int x = 0;
+            while (x<=4)
+            {
+                Console.WriteLine("Привет");
+                x++;
+            }
+        }
+    }
+}
