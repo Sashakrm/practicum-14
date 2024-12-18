9a90a0a063c74d812ba916b49e9d1281d0e98a65
Sashakrm <sasakramch@gmail.com>
2024-12-18 09:20:33 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Input("n= ");
+            Cicle(n);
+            Console.Read();
+        }
+        static int Input(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Cicle(int n)
+        {
+            int x = 1;
+            while (x <= n)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
